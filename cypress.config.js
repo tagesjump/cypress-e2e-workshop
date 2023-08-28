37a89eee7a665be9b362b630eb615326df0812e2
timur.yusupov <timur.yusupov@tages.ru>
2023-08-28 23:25:05 +0500

    first commit

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,8 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: 'https://www.saucedemo.com',
+    env: {
+      baseUrl: 'https://www.saucedemo.com',
+    }
   },
 });
