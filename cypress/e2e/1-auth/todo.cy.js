84d5162dec68c116a7374981967f56399a6afdbe
timur.yusupov <timur.yusupov@tages.ru>
2023-08-29 23:39:15 +0500

    add first test

--- cypress/e2e/1-auth/todo.cy.js
+++ cypress/e2e/1-auth/todo.cy.js
@@ -1,16 +1,11 @@
 
 describe('Авторизация', () => {
   beforeEach(() => {
-    // Cypress starts out with a blank slate for each test
-    // so we must tell it to visit our website with the `cy.visit()` command.
-    // Since we want to visit the same URL at the start of all our tests,
-    // we include it in our beforeEach function so that it runs before each test
-
     cy.visit('/')
 
   })
 
-  it('Вход под standart', () => {
+  it('Вход под test@mail.com', () => {
     cy.visit('/user/login/index')
     cy.get('#exampleInputEmail1').type('test@mail.com')
     cy.get('#exampleInputPassword1').type('1')
